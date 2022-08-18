a580532a0a987f34400373785c5d9b0ee3fef733
gaanti <gaskevich06@gmail.com>
2022-08-18 15:17:27 +0300

    Added sort functionality

--- src/main/java/com/example/pizza/pizza/PizzaDatabaseController.java
+++ src/main/java/com/example/pizza/pizza/PizzaDatabaseController.java
@@ -1,10 +1,12 @@
 package com.example.pizza.pizza;
 
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +17,20 @@ import java.util.List;
 public class PizzaDatabaseController {
     @Autowired
     PizzaRepository pizzaRepository;
+
     @GetMapping
     Page<Pizza> getAllPizzas(@RequestParam(required = false, name = "sortBy", defaultValue = "popularity") String sortBy,
-                             @RequestParam(required = false, name = "filterBy") Category category,
+                             @RequestParam(required = false, name = "filterBy", defaultValue = "") Category category,
                              @RequestParam(required = false, name = "page", defaultValue = "0") int page,
                              @RequestParam(required = false, name = "search", defaultValue = "") String search) {
-        Pageable pageable = PageRequest.of(page, 8, Sort.by(sortBy).ascending());
-        if (category != null) {
-            return pizzaRepository.findByCategory(category, pageable);
+        Pageable pageable = PageRequest.of(page, 8, Sort.by(sortBy).descending());
+
+        if (category != null && search != null) {
+            return pizzaRepository.findByTitleContainsIgnoreCaseAndCategory(search, category, pageable);
+        } else if (category != null) {
+            return pizzaRepository.findAllByCategory(category, pageable);
+        } else if (search != null) {
+            return pizzaRepository.findAllByTitleContainsIgnoreCase(search, pageable);
         } else return pizzaRepository.findAll(pageable);
     }
 }
--- src/test/java/com/example/pizza/PizzaApplicationTests.java
+++ src/test/java/com/example/pizza/PizzaApplicationTests.java
@@ -1,57 +1,28 @@
 package com.example.pizza;
 
 import com.example.pizza.pizza.Category;
-import com.example.pizza.pizza.DoughType;
 import com.example.pizza.pizza.Pizza;
+import com.example.pizza.pizza.PizzaDatabaseController;
 import com.example.pizza.pizza.PizzaRepository;
-import org.json.JSONException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.test.annotation.Commit;
-
-import javax.imageio.ImageIO;
-import javax.transaction.Transactional;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PizzaApplicationTests {
 	@Autowired
 	PizzaRepository pizzaRepository;
+	Pageable pageable = PageRequest.of(0, 3, Sort.by("price").ascending());
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	@Disabled
-	void readAndCreateImage() throws IOException {
-		Path path = Paths.get(
-				"/Users/juliagaskevich/IdeaProjects/XchangeTries/Second/backend/Pizza/src/main/java/com/example/pizza/pizza/pzimt.jpeg");
-		byte[] arr = Files.readAllBytes(path);
-		System.out.println(Arrays.toString(arr));
-		System.out.println(arr.length);
-
-
-		BufferedImage img = ImageIO.read(new ByteArrayInputStream(arr));
-		System.out.println("img = " + img.createGraphics());
-		ImageIO.write(img, "jpg", new File("output.jpg"));
-		System.out.println("image created");
 	}
 
 	@Test
@@ -64,13 +35,16 @@ class PizzaApplicationTests {
 		pizza.setCategory(Category.Grille);
 		pizza.setDoughType("[\"Thin\", \"Traditional\"]");
 		pizza.setPrice(400);
-		pizza.setRating(9);
 		Pizza saved = pizzaRepository.saveAndFlush(pizza);
 	}
 
 	@Test
 	void pageble() {
-		Pageable pageable = PageRequest.of(0, 3, Sort.by("price").ascending());
 		assertThat(pizzaRepository.findAll(pageable).getTotalPages()).isGreaterThan(2);
 	}
+
+	/*@Test
+	void search() {
+		assertThat(finded).isNotNull();
+	}*/
 }
