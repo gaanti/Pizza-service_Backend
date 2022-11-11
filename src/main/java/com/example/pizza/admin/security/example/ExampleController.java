46ff36216ef4f6d7558324f1bbe39a70d97a421d
gaanti <gaskevich06@gmail.com>
2022-11-11 10:37:02 +0200

    small improvements

--- src/main/java/com/example/pizza/admin/security/example/ExampleController.java
+++ src/main/java/com/example/pizza/admin/security/example/ExampleController.java
@@ -1,24 +1,37 @@
 package com.example.pizza.admin.security.example;
 
+import com.example.pizza.admin.security.user.JwtUser;
+import com.example.pizza.admin.security.user.JwtUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
+    @Autowired
+    JwtUserService jwtUserRepository;
 
-    @GetMapping("/user")
-    public ExampleMessage userEndpoint() {
-        return new ExampleMessage("Hello user!");
-    }
 
-    @GetMapping("/login")
-    public ExampleMessage loginEndpoint() {
-        return new ExampleMessage("!");
-    }
+	@GetMapping("/user")
+	public ExampleMessage userEndpoint() {
+		return new ExampleMessage("Hello user!");
+	}
 
-    @GetMapping("/admin")
-    public ExampleMessage adminEndpoint() {
-        return new ExampleMessage("Hello admin!");
-    }
+	@GetMapping("/login")
+	public ExampleMessage loginEndpoint() {
+		return new ExampleMessage("!");
+	}
+
+	@GetMapping("/admin")
+	public ExampleMessage adminEndpoint() {
+		return new ExampleMessage("Hello admin!");
+	}
+
+	@GetMapping("/check-db-data")
+	public JwtUser checkDbData(@RequestParam(value = "email") String email) {
+		return jwtUserRepository.getByEmail(email);
+	}
+
 
 }
